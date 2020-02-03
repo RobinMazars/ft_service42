049670a58feb10c7c2bd2b1234c81058653a7d50
Thais Marcon <tmarcon@z2r6p2.le-101.fr>
2020-02-03 11:05:39 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+minikube start --cpus=4 --memory 4000 --disk-size 11000 --vm-driver virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+
+minikube addons enable ingress
+
+export KUB_IP=$(minikube ip)
+
+eval $(minikube docker-env)
+
+docker build -t custom-nginx:1.11 srcs/nginx
+docker build -t custom-grafana srcs/grafana
+
+kubectl apply -f ingress.yaml
+kubectl apply -f nginx.yaml
+kubectl apply -f grafana.yaml
