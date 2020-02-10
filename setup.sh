22025450a449bbb3a0480fff55345fd9e6a8c41a
Robin Mazars <rmazars@z2r6p4.le-101.fr>
2020-02-10 09:37:11 +0100

    all datasource on grafana config

--- setup.sh
+++ setup.sh
@@ -6,14 +6,20 @@ export KUB_IP=$(minikube ip)
 
 eval $(minikube docker-env)
 
+cp srcs/grafana.yaml srcs/grafana_edit.yaml
+sed -i '' "s/_KUB_IP_/$KUB_IP/g" srcs/grafana_edit.yaml
+
+cp srcs/nginx/telegraf.conf srcs/nginx/telegraf_edit.conf
+sed -i '' "s/_KUB_IP_/$KUB_IP/g" srcs/nginx/telegraf_edit.conf
+
+cp srcs/phpmyadmin/telegraf.conf srcs/phpmyadmin/telegraf_edit.conf
+sed -i '' "s/_KUB_IP_/$KUB_IP/g" srcs/phpmyadmin/telegraf_edit.conf
+
 docker build -t custom-nginx srcs/nginx
 docker build -t custom-mysql srcs/mysql
 docker build -t custom-pma srcs/phpmyadmin
 docker build -t custom-ftps srcs/ftps
 docker build -t custom-wordpress srcs/wordpress
-
-cp srcs/grafana.yaml srcs/grafana_edit.yaml
-sed -i '' "s/_KUB_IP_/$KUB_IP/g" srcs/grafana_edit.yaml
 
 kubectl apply -f srcs/ingress.yaml
 kubectl apply -f srcs/nginx.yaml
@@ -22,5 +28,6 @@ kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/ftps.yaml
 kubectl apply -f srcs/grafana_edit.yaml
 kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/wordpress.yaml
 
 minikube service wordpress --url
